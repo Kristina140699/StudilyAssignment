92b3768bbfa375ad5cdcd0eebb1c6df1cd750623
Kristina <Kristina.11915169@lpu.in>
2022-01-16 21:08:40 +0530

    Added code of Homepage.

---
+++ 5StudilyHome.php
@@ -0,0 +1,40 @@
+<html>
+    <head>        <title>Optional Subject Details</title>
+
+<style>
+      body{background-image: url("blues.jpg");
+        width: 100%;
+        
+  
+  background-attachment: fixed;
+  }
+    a:link{
+        text-decoration: none;
+        color: white;}
+    a:hover{text-decoration: none;
+        color: blue;}
+    a:active{text-decoration: none;
+        color: black;}
+    a:visited{text-decoration: none;
+        color: purple;}
+
+                .submit{
+                    text-align:center;
+                    background: DodgerBlue;
+                    color: #fff;
+                    font-weight: bold;
+                    border-radius: 4px;
+                    border: none;
+                    font-size: 20px;
+                }
+</style></head>
+<body style="margin-top:10%;"><center>
+    <h1>Welcome to Studily Home</h1>
+<div style="background-color:rgb(255, 255, 255, 0.6);
+       width:88%; margin:20px;padding: 15px;"><br/>
+       <button class="submit"><a href="0Studilyfetch1.php">View Teacher Details</a></button><br/><br/><br/>
+       <button class="submit"><a href="2StudilyOPsubjectForm.php">Select your Optional Subjects</a></button><br/><br/><br/>
+       <button class="submit"><a href="3Studilyfetch1.php">Optional Subject Details</a></button><br/><br/>
+       </div></center>
+</body>
+       </html>
